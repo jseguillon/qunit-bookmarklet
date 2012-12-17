44edbcdb2dd6f6b9b6483f5a8981347bf15c3cb2
Joel Seguillon <joel.seguillon@niji.fr>
2012-12-17 18:23:59 +0100

    Last Try

--- qunit-bookmarklet.js
+++ qunit-bookmarklet.js
@@ -31,32 +31,32 @@ function loadScript(url, callback)
 // This code loads jQuery and executes some code when jQuery is loaded
 loadScript("http://code.jquery.com/jquery-latest.js", function()
 {
-	$('my_element').hide();
-});
+	//Add qunit divs
+	$('body').append('<div id="qunit"></div><div id="qunit-fixture"></div>');
+	//Add Qunit and jquery ui css
+	$('head').append('<link rel="stylesheet" href="http://code.jquery.com/qunit/qunit-git.css" type="text/css" />');
+	$('head').append('<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />');
+	//Style qunit div : non 100% width and z-index to 32bits max int
+	$('head').append('<style>#qunit { width: 600px; z-index: 2147483647; position: fixed; top: 0; left: 0;}</style>');
 
-//Add qunit divs
-$('body').append('<div id="qunit"></div><div id="qunit-fixture"></div>');
-//Add Qunit and jquery ui css
-$('head').append('<link rel="stylesheet" href="http://code.jquery.com/qunit/qunit-git.css" type="text/css" />');
-$('head').append('<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />');
-//Style qunit div : non 100% width and z-index to 32bits max int
-$('head').append('<style>#qunit { width: 600px; z-index: 2147483647; position: fixed; top: 0; left: 0;}</style>');
-
- 
-//Get Qunit
-$.getScript('http://code.jquery.com/qunit/qunit-git.js').done(function(script, textStatus) {
-	//Get jquery UI to get draggable and resizable
-	$.getScript('http://code.jquery.com/ui/1.9.2/jquery-ui.js').done (function(script, textStatus) {
-		//Init and start (but why ?) 
-		QUnit.init();QUnit.start();
-		$(function() {
-			$( "#qunit" ).draggable().resizable({ handles: "e, w" });;
+	 
+	//Get Qunit
+	$.getScript('http://code.jquery.com/qunit/qunit-git.js').done(function(script, textStatus) {
+		//Get jquery UI to get draggable and resizable
+		$.getScript('http://code.jquery.com/ui/1.9.2/jquery-ui.js').done (function(script, textStatus) {
+			//Init and start (but why ?) 
+			QUnit.init();QUnit.start();
+			$(function() {
+				$( "#qunit" ).draggable().resizable({ handles: "e, w" });;
+			});
+			
+			//Check if known script to run, based on local storage
+			if(typeof (localStorage.qunitScript) !== 'undefined') { $.getScript(localStorage.qunitScript); } 
+			else { console.log("No script to run, please add one using localStorage.qunitScript = 'scriptURL' in your debug console. \n Example : localStorage.qunitScript='https://raw.github.com/jseguillon/qunit-bookmarklet/master/sample.js' \n You still can test using qunit test() syntax."); }
+			
 		});
-		
-		//Check if known script to run, based on local storage
-		if(typeof (localStorage.qunitScript) !== 'undefined') { $.getScript(localStorage.qunitScript); } 
-		else { console.log("No script to run, please add one using localStorage.qunitScript = 'scriptURL' in your debug console. \n Example : localStorage.qunitScript='https://raw.github.com/jseguillon/qunit-bookmarklet/master/sample.js' \n You still can test using qunit test() syntax."); }
-		
 	});
 });
+
+
 
