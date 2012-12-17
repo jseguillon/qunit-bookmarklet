da4a664a3bcb418d655191c889a81d543bad0186
jseguillon <joel.seguillon@gmail.com>
2012-12-17 22:05:44 +0100

    More descriptive example

--- sample.js
+++ sample.js
@@ -1,5 +1,10 @@
-test( "default test", function() {
-		ok( 1 == "1", "Default test injected from https://raw.github.com/jseguillon/qunit-bookmarklet/master/sample.js");
+module("default tests");
+test( "injected from https://raw.github.com/jseguillon/qunit-bookmarklet/master/sample.js", function() {
 		ok( 1 == "1", "Its working" );
 		ok( 1 == "1", "Look at your console" );
+});
+
+module("features added");
+test( "added draggable and sizable", function() {
+		ok( 1 == "1", "Now you can move and resize your qunit result bar (horizontal only)" );
 });
